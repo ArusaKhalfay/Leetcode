cdb1f73066fc9b8edeecf3e19edee38cf341ebc8
ArusaKhalfay <156254801+ArusaKhalfay@users.noreply.github.com>
2025-02-14 08:54:49 -0500

    Time: 15 ms (43.16%), Space: 17.7 MB (95.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,23 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s1 = {}
-        t1 = {}
+        d1 = {}
+        d2 = {}
+
         for i in range(0, len(s)):
-            if s[i] in s1:
-                s1[s[i]] += 1
-            elif s[i] not in s1:
-                s1[s[i]] = 1
-        
-        for j in range(0, len(t)):
-            if t[j] in t1:
-                t1[t[j]] += 1
+            if s[i] in d1:
+                d1[s[i]] += 1
             else:
-                t1[t[j]] = 1
+                d1[s[i]] = 1
 
-        if s1 == t1:
-            return True
-        else:
-            return False
-
+        for j in range(0, len(t)):
+            if t[j] in d2:
+                d2[t[j]] += 1
+            else:
+                d2[t[j]] = 1
+        
+        return d1 == d2
 
         

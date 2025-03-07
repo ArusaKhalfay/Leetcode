dcc2a30eb3b517216cc878efd176d899749cb853
ArusaKhalfay <156254801+ArusaKhalfay@users.noreply.github.com>
2025-03-06 18:00:09 -0800

    Time: 15 ms (88.61%), Space: 19.9 MB (12.14%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,26 +1,6 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        d = {}
+        num_candy_types = len(set(candyType))
+        n = len(candyType)//2
 
-        for i in range(0, len(candyType)):
-            if candyType[i] in d:
-                d[candyType[i]] += 1
-            else:
-                d[candyType[i]] = 1
-
-        n = len(candyType)
-        allowed_candy = n//2
-        print(allowed_candy)
-        
-        all_candy_type = len(d.values())
-        print(all_candy_type)
-        
-        if allowed_candy == all_candy_type:
-            return all_candy_type
-        elif allowed_candy > all_candy_type:
-            return all_candy_type
-        elif allowed_candy < all_candy_type:
-            return allowed_candy
-         
-
-        
+        return min(n, num_candy_types)

92655106300f44b79a83e530c8c6269db68d90d1
ArusaKhalfay <156254801+ArusaKhalfay@users.noreply.github.com>
2025-03-09 07:00:29 -0700

    Time: 4 ms (46.18%), Space: 18.3 MB (44.69%) - LeetHub

--- 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.py
+++ 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.py
@@ -3,21 +3,23 @@ class Solution:
         d1 = {}
         d2 = {}
 
-        for i in range(0, len(words1)):
-            if words1[i] in d1:
-                d1[words1[i]] += 1
+        for word in words1:
+            if word in d1:
+                d1[word] += 1
             else:
-                d1[words1[i]] = 1
+                d1[word] = 1
+        
 
-        for i in range(0, len(words2)):
-            if words2[i] in d2:
-                d2[words2[i]] += 1
+        for word in words2:
+            if word in d2:
+                d2[word] += 1
             else:
-                d2[words2[i]] = 1
+                d2[word] = 1
+
         
         count = 0
-        for word in d1:
-            if d1[word] == 1 and word in d2 and d2[word] == 1:
+        for word, value in d1.items():
+            if value == 1 and word in d2 and d2[word] == 1:
                 count += 1
         
-        return count
+        return count 
